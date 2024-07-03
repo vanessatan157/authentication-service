f95f7e33052554131e1ba3fe30031ddcd29623e4
wqyeo <yeo_dev@protonmail.com>
2024-07-04 02:28:48 +0800

    Project init

---
+++ src/test/java/muimi/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package muimi.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void starterTest() {
+        assertEquals(1, 1);
+    }
+
+}
